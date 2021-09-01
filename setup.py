0738306cc8deda0983ce466dd835099349375221
Pantelis Mitropoulos <pmitropoulos@getmap.gr>
2021-09-01 18:57:01 +0300

    Version 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bigdatavoyant",
-    version="1.2.0",
+    version="1.2.4",
     author="Pantelis Mitropoulos",
     author_email="pmitropoulos@getmap.gr",
     description="Geodata profiling tool",
